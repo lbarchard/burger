32a31139935b2ab74b7354e66ad7ae2e32996335
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-04-30 11:56:46 -0400

    removed local connection info

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
-devoured boolean DEFAULT false,
-date timestamp DEFAULT NOW(),
+burger_name VARCHAR(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP DEFAULT NOW(),
 PRIMARY KEY (id)
 );
