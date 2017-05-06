dedd6853984e88481c9a58fba0f538307a737646
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-05-06 06:54:21 -0400

    Got basic bootstrap going

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 var mysql       = require("mysql");
-// var secureInfo  = require("./secureInfo.js");
+var secureInfo  = require("./secureInfo.js");
 
 var connection
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: secureInfo.password,
-//     database: "burgers_db"
-// });
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: secureInfo.password,
+    database: "burgers_db"
+});
 };
  
 
